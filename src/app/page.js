550e2f23fc1bd9a54998640fe0f7cd7e027f4594
Shubham Tingare <shubhamtingare0661@gmail.com>
2024-06-30 10:59:53 +0530

    project done

---
+++ src/app/page.js
@@ -0,0 +1,11 @@
+import Table from "../components/Table";
+
+export default function Home() {
+  return (
+    <div>
+      <main className="min-h-screen bg-gray-100 p-8">
+        <Table />
+      </main>
+    </div>
+  );
+}
